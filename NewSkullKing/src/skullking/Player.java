3ddaf31b7ddc3151b183d10e044206dfaa83f0d6
moon0331 <moon0331@skku.edu>
2018-06-11 14:33:03 +0900

    player 점수계산 올림

--- NewSkullKing/src/skullking/Player.java
+++ NewSkullKing/src/skullking/Player.java
@@ -3,6 +3,39 @@ package skullking;
 import javax.swing.JPanel;
 
 public class Player extends JPanel{
+	static String[] nameString= {"AAA","BBB","CCC","DDD","FFF"};
 	String name;
+	int predictWin;
+	int numOfWin;
+	int idx;
 	int score;
+	public Player(int i) {
+		idx=i;
+		name=nameString[i]; //exception
+	}
+	
+	public void setPredictWin(int win) {
+		predictWin=win;
+	}
+	
+	public int setWin() {
+		numOfWin++;
+		return numOfWin;
+	}
+	
+	public int calScore(int round) {
+		int val=0;
+		if(predictWin==0) {
+			if(numOfWin==0) val=10*round;
+			else val=(-10)*round;
+		} else if(predictWin==numOfWin) val=20*predictWin;
+		else val=Math.abs(predictWin-numOfWin)*(-10);
+		return val;
+	}
+	
+	public void updateScore(int round) {
+		int s=calScore(round);
+		System.out.println(name+" score +"+s);
+		score+=s;
+	}
 }
